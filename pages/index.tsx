cc52368c12aa9f1f75741a7af1d690b443d41589
tomasbond02 <bondtomy@gmail.com>
2023-11-15 15:38:50 -0300

    agregar clima

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,17 @@ const Home: NextPage = () => {
           <h2>Animate a vivir una experiencia inmersiva.</h2>
         </div>
       </header>
+      <div className={styles.clima}>
+      <iframe src="https://www.meteoblue.com/es/tiempo/widget/daily?geoloc=detect&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&pressure=0&layout=light"   scrolling="NO" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox"></iframe>
+      </div>
       <main className={styles.main}>
         <Image src='/PATIO.png'
         alt='cronograma'
         width={600}
         height={800}
-        />
+      />
       </main>
-    </div>
+          </div>
   )
 }
 
